33ee7fd694083f21ff22d2922edeb7ab0f774e66
Sayan Das <dsayan154@gmail.com>
2022-10-01 20:29:50 +0530

    created wordpress vpc

--- assignment_1/src/terraform/provider.tf
+++ assignment_1/src/terraform/provider.tf
@@ -1,14 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.33.0"
     }
   }
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region  = "ap-south-1"
   profile = "sayan-local-admin"
+  default_tags {
+    tags = {
+      "terraform" = "true"
+      "project"   = "kpmg-assignment-1-wordpress"
+    }
+  }
 }
 
